4846aec4e20c0195d47efe3828ef6c716a611249
Ankitsharma0001 <ankitbhardoaj@gmail.com>
2020-08-14 15:43:06 +0530

    Assignment 1

---
+++ Assignment1/hard.js
@@ -0,0 +1,23 @@
+console.log("=====Q1. Get Real Type=====");
+
+function realType(value) {
+  let arr = Object.prototype.toString.call(value);
+  let type = arr.split(' ');
+  let typeStr = '';
+
+  for (i = 0; i < type[1].length; i++) {
+    if (type[1].charAt(i) !== ']') typeStr += type[1].charAt(i);
+  }
+
+  return typeStr.toLocaleLowerCase();
+}
+
+console.log(realType(1));
+console.log(realType('a'));
+console.log(realType(true));
+console.log(realType([]));
+console.log(realType(null));
+
+
+console.log("=======Q2) Numbers in Strings=========");
+
